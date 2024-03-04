3229b384db828b2da9b7fd5f772c2087904523f2
Alexander1987r <alexnext7771987r@rambler.ru>
2024-03-05 00:00:18 +0300

    changes in the request

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 import './Sort/sort.js';
 import './Upload/uploadFile.js';
 import {closeUploadPicture} from './Upload/uploadFile.js'
-import { renderingPhotos } from './Pictures/pictures.js';
+import { renderingPhotos,photoDescriptionArr} from './Pictures/pictures.js';
 import { setUserFormSubmit } from './Validation/validation.js';
 import { getBigPicture } from './Big-picture/bigPicture.js';
+import { showAlert } from './Utils/utils.js';
 
 
-fetch('https://25.javascript.htmlacademy.pro/kekstagram/data')
-.then((responce)=>responce.json())
-.then((descriptionArray)=>{
-  renderingPhotos(descriptionArray);
-  getBigPicture(descriptionArray);
+fetch('https://25.javascript.htmlacademy.pro/kekstagram/dat')
+.then((responce)=>{
+  if(responce.ok){
+    return responce.json();
+  } else {
+    renderingPhotos(photoDescriptionArr);
+    getBigPicture(photoDescriptionArr);
+    throw new Error('Сервер не доступен.Идет загрузка демо-версии');
+  }
 })
+.then((responce)=>{
+  renderingPhotos(responce);
+  getBigPicture(responce);
+})
+.catch((err)=>showAlert(err.message))
 
 setUserFormSubmit(closeUploadPicture);
